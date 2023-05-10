8675cf0640b3341a1b6d8f42778dd7e49fae5578
David Ruiz <david.ruizluque@usp.ceu.es>
2023-05-10 20:19:14 +0200

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,16 @@
 import heapq
 from collections import defaultdict
+
+#Tabla de frecuencias
+caracter_cantidad = {
+    'A': 11, 'B': 2, 'C': 4, 'D': 3, 'E': 14, 'G': 3, 'I': 6, 'L': 6,
+    'M': 3, 'N': 6, 'O': 7, 'P': 4, 'Q': 1, 'R': 10, 'S': 4, 'T': 3,
+    'U': 4, 'V': 2, ' ': 17, ',': 2
+}
+
+# Calcular frecuencias
+total_caracteres = sum(caracter_cantidad.values())
+frecuencias = {caracter: cantidad / total_caracteres for caracter, cantidad in caracter_cantidad.items()}
+
+#Arbol de Huffman
+
