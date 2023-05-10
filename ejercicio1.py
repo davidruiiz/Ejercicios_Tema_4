e45ef01d48ce8ed1d01ee3dcf3c27fda75063903
David Ruiz <david.ruizluque@usp.ceu.es>
2023-05-10 20:19:38 +0200

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -14,3 +14,15 @@ frecuencias = {caracter: cantidad / total_caracteres for caracter, cantidad in c
 
 #Arbol de Huffman
 
+def crear_arbol_huffman(frecuencias):
+    heap = [[peso, [caracter, ""]] for caracter, peso in frecuencias.items()]
+    heapq.heapify(heap)
+    while len(heap) > 1:
+        lo = heapq.heappop(heap)
+        hi = heapq.heappop(heap)
+        for pair in lo[1:]:
+            pair[1] = '0' + pair[1]
+        for pair in hi[1:]:
+            pair[1] = '1' + pair[1]
+        heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+    return sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
